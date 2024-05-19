4160dcdf8dfeb62fcc0c1a001719eb2e69f0c5aa
SyQuy27683 <130484427+SyQuy27683@users.noreply.github.com>
2024-05-19 21:13:20 +0700

    aa

--- app/src/main/java/com/example/foodapp/activity/MainActivity.java
+++ app/src/main/java/com/example/foodapp/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.foodapp;
+package com.example.foodapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.foodapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
