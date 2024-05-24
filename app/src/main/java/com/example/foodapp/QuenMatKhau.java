100595f7524ba40ae17929dff26f05d523e2a713
SyQuy27683 <130484427+SyQuy27683@users.noreply.github.com>
2024-05-25 02:43:53 +0700

    reset Pass

--- app/src/main/java/com/example/foodapp/QuenMatKhau.java
+++ app/src/main/java/com/example/foodapp/QuenMatKhau.java
@@ -5,13 +5,13 @@ import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ResetPassword extends AppCompatActivity {
+public class QuenMatKhau extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_reset_password);
+        setContentView(R.layout.activity_quen_mat_khau);
 
     }
 }
